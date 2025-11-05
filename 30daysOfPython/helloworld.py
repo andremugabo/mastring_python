05eb53dec87b7f58a312da380ebda0c081caeb67
Mugabo Andre <andremugabo@yahoo.fr>
2025-11-05 14:48:33 +0200

    first file helloworld.py

---
+++ 30daysOfPython/helloworld.py
@@ -0,0 +1,12 @@
+# This is the first comment 
+# Thi is the second comment
+# This is the third comment
+
+""" This is multiline comment multiline 
+comment takes multiple lines
+python is eating the world """
+
+# Day 1 - 30 days of python Challenge 
+
+print(2 + 3)   # Addition(+)
+print(3 - 1)  #subtraction(-)
